44d6fd7609151dc32de1af164a7801dc2e25cd48
ldunhao <ldunhao@hotmail.com>
2019-11-16 14:58:05 -0200

    Ex1

--- Exercicios/Expressions/Vrfs/Ex1.hs
+++ Exercicios/Expressions/Vrfs/Ex1.hs
@@ -28,6 +28,31 @@ verificaCmd vars (Seq c1 c2)   = (v2,       b1 && b2) where
                          (v1,b1) = verificaCmd vars c1
                          (v2,b2) = verificaCmd v1   c2
 
+type Mem = [(String, Int)]
+
+cosulta :: Mem -> String -> Int
+cosulta [] id = 0
+cosulta ((id',v'):l) id = if id == id' then
+                            v'
+                        else cosulta l id
+
+escreve :: Mem -> String -> Int -> Mem
+escreve mem id v = (id,v):mem
+
+avaliaExp :: Mem -> Exp -> Int
+avaliaExp mem (Num x) = x
+avaliaExp mem (Add e1 e2) = (avaliaExp mem e1) + (avaliaExp mem e2)
+avaliaExp mem (Sub e1 e2) = (avaliaExp mem e1) - (avaliaExp mem e2)
+avaliaExp mem (Var id) = cosulta mem id
+
+avaliaCmd :: Mem -> Cmd -> Mem
+avaliaCmd mem (Dcl _) = mem
+avaliaCmd mem (Atr id exp) = escreve mem id v where v = avaliaExp mem exp 
+avaliaCmd mem (Seq c1 c2) = avaliaCmd mem' c2 where mem' = avaliaCmd mem c1
+avaliaCmd mem (Cnd exp c1 c2) = if (avaliaExp mem exp) /= 0 then 
+                                    avaliaCmd mem c1
+                                else 
+                                    avaliaCmd mem c2
 
 p0 :: Cmd
 p0 = Seq (Atr "x" (Num 10))(Seq (Atr "x" (Num 20))(Atr "y" (Var "x")))
@@ -36,6 +61,6 @@ p2 = Seq (Dcl "y") p1
 
 pe = Atr "x" (Num 10)
 
---main = print (verificaCmd [] p2)
-main = print (verificaCmd [] pe)
+main = print (verificaCmd [] p2)
+--main = print (verificaCmd ["y"] p0)
 --main = print (avaliaCmd [] p2)
