5d31d2e77b2478ee58d36a861fcb58445e98425d
Lucas <ldunhao@hotmail.com>
2019-11-18 08:19:30 -0300

    Ex1

--- Exercicios/Expressions/Func/Ex1.hs
+++ Exercicios/Expressions/Func/Ex1.hs
@@ -2,7 +2,80 @@
 -- Exercicio 1
 -- ======================================
 
+import Debug.Trace
 
-x = 1
-y = 2
-main = print (x+y)
+type Mem = [(String,Int)]
+type Cod = [(String,Cmd)]
+type Env = (Mem,Cod)
+
+data Cmd = Atr String Exp
+         | Prt Exp
+         | Seq Cmd Cmd
+         | Cnd Exp Cmd Cmd
+         | Fun String Cmd
+  deriving Show
+
+
+data Exp = Num Int
+         | Add Exp Exp
+         | Sub Exp Exp
+         | Var String
+         | App String Exp
+  deriving Show
+
+consulta :: [(String,a)] -> String -> a
+consulta []           id = undefined
+consulta ((id',v'):l) id = if id == id' then
+                            v'
+                           else
+                            consulta l id
+
+escreve :: [(String,a)] -> String -> a -> [(String,a)]
+escreve l id v = (id,v):l
+
+avaliaExp :: Env -> Exp -> Int
+avaliaExp _         (Num v)     = v
+avaliaExp env       (Add e1 e2) = (avaliaExp env e1) + (avaliaExp env e2)
+avaliaExp env       (Sub e1 e2) = (avaliaExp env e1) - (avaliaExp env e2)
+avaliaExp (mem,_)   (Var id)    = consulta mem id
+avaliaExp (mem,cod) (App id e)  = ret where
+                                    ret = consulta mem'' "ret"
+                                    (mem'',_) = avaliaCmd (mem',cod) fun
+                                    mem' = escreve mem "arg" arg
+                                    arg  = avaliaExp (mem,cod) e
+                                    fun  = consulta cod id
+
+avaliaCmd :: Env -> Cmd -> Env
+avaliaCmd env       (Prt e)         = traceShow (avaliaExp env e) env
+avaliaCmd (mem,cod) (Atr id exp)    = (escreve mem id v,cod) where
+                                        v = avaliaExp (mem,cod) exp
+avaliaCmd env       (Seq c1 c2)     = avaliaCmd env' c2 where
+                                        env' = avaliaCmd env c1
+avaliaCmd env       (Cnd exp c1 c2) = if (avaliaExp env exp) /= 0 then
+                                        avaliaCmd env c1
+                                      else
+                                        avaliaCmd env c2
+avaliaCmd (mem,cod) (Fun id c)      = (mem, escreve cod id c)
+
+
+p1 = Seq (Seq (Atr "x" (Num 1)) (Atr "y" (Num 2))) (Prt (Add (Var "x") (Var "y")))
+
+p2 = Seq
+      (Fun "duplica"
+        (Atr "ret"
+          (Add (Var "arg")
+               (Var "arg"))))
+      (Prt (App "duplica" (Num 10)))
+
+p3 = Seq
+      (Fun "soma"
+        (Cnd (Var "arg")
+          (Atr "ret"
+            (Add (Var "arg")
+                 (App "soma"
+                   (Sub (Var "arg")
+                        (Num 1)))))
+          (Atr "ret" (Num 0))))
+      (Prt (App "soma" (Num 10)))
+
+main = print (avaliaCmd ([],[]) p1)
