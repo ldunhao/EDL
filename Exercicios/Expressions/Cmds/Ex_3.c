382fa46ef9166b9b17bcb457e76cf1db425faa0c
Lucas Dunhão de Carvalho <ldunhao@hotmail.com>
2019-12-01 22:39:09 -0300

    Update Ex_3.c

--- Exercicios/Expressions/Cmds/Ex_3.c
+++ Exercicios/Expressions/Cmds/Ex_3.c
@@ -10,23 +10,25 @@ struct Exp {
         Num, Add, Sub
     }type;
 
-    union valores {
+    union {
         int val;
-        struct Exp *exp1, *exp2;
-    }valores;
+        struct {
+            struct Exp *exp1, *exp2;
+        };
+    };
     
 };
 
 int avalia(struct Exp e){
     switch(e.type){
         case Num:
-            return e.valores.val;
+            return e.val;
             break;
         case Add:
-            return avalia(*(e.valores.exp1)) + avalia(*(e.valores.exp2));
+            return avalia(*(e.exp1)) + avalia(*(e.exp2));
             break;
         case Sub:
-            return avalia(*(e.valores.exp1)) - avalia(*(e.valores.exp2));
+            return avalia(*(e.exp1)) - avalia(*(e.exp2));
             break;
         default:
             break;
@@ -34,14 +36,14 @@ int avalia(struct Exp e){
 }
 
 int main(void){
-    struct Exp e1, e2, e3, num1, num2;
-    // Types: 0 = Num, 1 = Soma, 2 = Sub
-    e1.type = 0; e1.valores.val = 1;
-    num1.type = 0; num1.valores.val = 2;
-    num2.type = 0; num2.valores.val = 5;
-    e2.type = 1; e2.valores.exp1 = &num2; e2.valores.exp2 = &num1;
-    e3.type = 1; e3.valores.exp1 = &e1; e3.valores.exp2 = &e2;
-    printf("%d\n", avalia(e3));
+
+    struct Exp num1, num2, num3, soma, sub;
+    num1.type = 0, num1.val = 1;
+    num2.type = 0, num2.val = 2;
+    num3.type = 0, num3.val = 5;
+    soma.type = 1, soma.exp1 = &num1, soma.exp2 = &num2;
+    sub.type = 2, sub.exp1 = &soma, sub.exp2 = &num3;
+    printf("%d\n", avalia(soma));
     
     return 0;
 }
